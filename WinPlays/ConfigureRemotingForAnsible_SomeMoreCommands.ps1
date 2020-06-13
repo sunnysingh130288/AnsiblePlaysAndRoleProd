73c0926b25ff1f6c7c22b7b7058a0699d69125f6
sunnysingh1302 <48749770+sunnysingh1302@users.noreply.github.com>
2020-06-13 19:09:29 +0530

    Add files via upload

---
+++ WinPlays/ConfigureRemotingForAnsible_SomeMoreCommands.ps1
@@ -0,0 +1,15 @@
+﻿NET USER ansible_user "Passw0rd" /ADD
+NET LOCALGROUP "Administrators" "ansible_user" /ADD
+Set-Item -Path WSMan:\localhost\Service\Auth\Basic -Value $true
+Set-Item -Path WSMan:\localhost\Service\AllowUnencrypted -Value $true
+netsh advfirewall firewall add rule name="WinRM" dir=in action=allow protocol=TCP localport=5985 remoteip=10.0.0.15
+
+#################If some network related issue occurs######################
+$networkListManager = [Activator]::CreateInstance([Type]::GetTypeFromCLSID([Guid]"{DCB00C01-570F-4A9B-8D69-199FDBA5723B}")) 
+$connections = $networkListManager.GetNetworkConnections() 
+
+# Set network location to Private for all networks 
+$connections | % {$_.GetNetwork().SetCategory(1)}
+
+###################COnnection Check#######################
+winrm identify -u:ansible_user -p:Passw0rd -r:http://localhost:5985
